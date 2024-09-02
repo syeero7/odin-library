88f551f751ed4420d4ceec298b1da5eb6bf6c378
SyeerO7 <EmalHasitha@proton.me>
2024-09-02 18:58:49 +0530

    Add a function to get user input

--- script.js
+++ script.js
@@ -1,10 +1,18 @@
 const bookList = document.querySelector("#bookList");
+const newBookBtn = document.querySelector("#new-book");
+const dialog = document.querySelector("dialog");
+const closeBtn = dialog.querySelector("#close-btn");
+const form = dialog.querySelector("form");
 
 const myLibrary = [
   { title: "borrow", author: "jim board", pages: 53, status: true },
   { title: "zorro", author: "tom jest", pages: 22, status: false },
   { title: "ages", author: "ice cold", pages: 33, status: true },
 ];
+
+newBookBtn.addEventListener("click", () => dialog.showModal());
+closeBtn.addEventListener("click", () => dialog.close());
+form.addEventListener("submit", getUserInput);
 
 function Book(title, author, pages, status) {
   this.title = title;
@@ -24,4 +32,16 @@ function displayBooks() {
     listItem.textContent = `${book.title} by ${book.author}, ${book.pages} pages ${book.status ? "Read" : "Not read yet"}`;
     bookList.appendChild(listItem);
   });
+}
+
+function getUserInput(e) {
+  e.preventDefault();
+
+  const title = form.querySelector("#title").value;
+  const author = form.querySelector("#author").value;
+  const pages = form.querySelector("#pages").value;
+  const status = form.querySelector("#read-status").checked ? true : false;
+
+  addBookToLibrary(title, author, pages, status);
+  form.reset();
 }
