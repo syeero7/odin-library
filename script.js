3a331aacb53d66e7b838f0fbd7ff3a57347cd1d8
SyeerO7 <EmalHasitha@proton.me>
2024-09-02 17:44:01 +0530

    Add a function to display books in myLibrary

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
-const myLibrary = [];
+const bookList = document.querySelector("#bookList");
+
+const myLibrary = [
+  { title: "borrow", author: "jim board", pages: 53, status: true },
+  { title: "zorro", author: "tom jest", pages: 22, status: false },
+  { title: "ages", author: "ice cold", pages: 33, status: true },
+];
 
 function Book(title, author, pages, status) {
   this.title = title;
@@ -10,4 +16,12 @@ function Book(title, author, pages, status) {
 function addBookToLibrary(title, author, pages, status) {
   const newBook = new Book(title, author, pages, status);
   myLibrary.push(newBook);
+}
+
+function displayBooks() {
+  myLibrary.forEach((book, index) => {
+    const listItem = document.createElement("li");
+    listItem.textContent = `${book.title} by ${book.author}, ${book.pages} pages ${book.status ? "Read" : "Not read yet"}`;
+    bookList.appendChild(listItem);
+  });
 }
